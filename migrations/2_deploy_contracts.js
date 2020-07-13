3036ebbc6f5e223361cd06b2b844071ae17047f2
gumshed <gumshed@protonmail.com>
2020-07-13 01:54:03 -0600

    s

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,6 +9,6 @@ module.exports = function (deployer) {
     deployer.deploy(MovementToken, 15, 0);
 
     //for test contracts
-    deployer.deploy(ERC20DemoToken, "");
-    deployer.deploy(ERC20DemoCallBackToken, "");
+    deployer.deploy(ERC20DemoToken, "0x101104b24Ba6CFE2105acdae4479f7887e0A48B3");
+    deployer.deploy(ERC20DemoCallBackToken, "0x7A35561e25D088577f0c31d3a233F407c3D43AB4");
 };
